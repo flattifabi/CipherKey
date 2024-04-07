a09bf907e9ddbd965e670bc9ccbba9c0c7c9afeb
Fabian Flattinger <fabiflatti@gmail.com>
2024-04-07 13:35:28 +0200

    Added CipherKey Cloud

--- CipherInstall/MainWindow.xaml.cs
+++ CipherInstall/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
@@ -77,14 +78,12 @@ public partial class MainWindow : FluentWindow, INotifyPropertyChanged
         
     }
     
-    
     public event PropertyChangedEventHandler? PropertyChanged;
 
     protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
-
     protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
     {
         if (EqualityComparer<T>.Default.Equals(field, value)) return false;
